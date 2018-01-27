c005d1f180d5cf4dedc73aa1ba2197d0e75feaad
mikeh <mike.hornegger@gmail.com>
2018-01-27 09:30:14 +0100

    removed unused imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
