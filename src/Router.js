7825a4b1111a24a200c711f0a2242b92dd0ae9db
mikeh <mike.hornegger@gmail.com>
2018-01-31 17:32:54 +0100

    created dummy EmployeeList

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './Components/LoginForm';
-
+import EmployeeList from './Components/EmployeeList';
 
 const RouterComponent = () => (
     <Router>
       <Scene key="root">
         <Scene key="login" component={LoginForm} title="Please Login" />
+        <Scene
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+        />
       </Scene>
     </Router>
   );
