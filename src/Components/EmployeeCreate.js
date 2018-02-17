47d3d588ebf397c903273391e4bf3190c512b6ec
mikeh <mike.hornegger@gmail.com>
2018-02-17 07:02:40 +0100

    Create employeeCreate component, reducer and action

--- src/Components/EmployeeCreate.js
+++ src/Components/EmployeeCreate.js
@@ -1,14 +1,78 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Picker, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { Card, CardSection, Input, Button } from './common';
+import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={
+              value => this.props.employeeUpdate({ prop: 'name', value })
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="0043 666 / 333 44 55"
+            value={this.props.phone}
+            onChangeText={
+              value => this.props.employeeUpdate({ prop: 'phone', value })
+            }
+          />
+        </CardSection>
+
+        <CardSection style={styles.pickerCardSection}>
+          <Text style={styles.pickerLabel}>Shift</Text>
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={
+              value => this.props.employeeUpdate({ prop: 'shift', value })
+            }
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+
+      </Card>
     );
   }
 }
 
-export default EmployeeCreate;
+const styles = {
+  pickerLabel: {
+    fontSize: 18,
+    paddingLeft: 20
+  },
+  pickerCardSection: {
+    flexDirection: 'column'
+  }
+};
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
---
+++ src/actions/AuthActions.js
@@ -0,0 +1,49 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import {
+  EMAIL_CHANGED,
+  PASSWORT_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER
+} from './types.js';
+
+export const emailChanged = (text) => (
+  {
+    type: EMAIL_CHANGED,
+    payload: text
+  }
+);
+
+export const passwordChanged = (text) => (
+  {
+    type: PASSWORT_CHANGED,
+    payload: text
+  }
+);
+
+export const loginUser = ({ email, password }) => (dispatch) => {
+  dispatch({ type: LOGIN_USER });
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => loginUserSuccess(dispatch, user))
+    .catch((error) => {
+      console.log(error);
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => loginUserFail(dispatch));
+    });
+  };
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+
+  Actions.main();
+};
